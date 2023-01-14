a8d5bfa99683057cea4fa90b9308aeccdd86ad81
Athithyan Rengaswamy <atr@Athithyans-MBP.bbrouter>
2023-01-14 23:31:48 +0530

    added get ss route

--- index.js
+++ index.js
@@ -1,47 +1,53 @@
-Object.assign(global, { components: {}, log: (...args) => console.log(...args) })
+Object.assign(global, {
+  components: {},
+  log: (...args) => console.log(...args),
+});
 
 //load env
-import dotenv from 'dotenv'
-dotenv.config()
-global.log('loaded env file')
-
+import dotenv from "dotenv";
+dotenv.config();
+global.log("loaded env file");
 
 // start server
-import Fastify from 'fastify'
-import fs from 'fs'
+import Fastify from "fastify";
+import fs from "fs";
 
-import { Puppy } from './puppy.js'
+import { Puppy } from "./puppy.js";
+import { constants } from "./utils.js";
 
-const server = Fastify({ logger: true })
+const server = Fastify({ logger: true });
 
-const constants = {
-  url: 'https://dev.to/',
-  props: { waitUntil: 'networkidle0' },
-  ss_name: 'screenshot.jpg'
-}
-
-server.get('/', async (request, reply) => {
+server.get("/get-ss", async (_request, reply) => {
   try {
-    const puppy = await Puppy.create()
-    await puppy.goto(constants.url, { props: constants.props })
-    await puppy.ss(constants.ss_name)
-    await puppy.close()
-    const stream = fs.createReadStream(constants.ss_name)
-    reply.header('Content-Type', 'image/jpg')
-    reply.send(stream)
+    const stream = fs.createReadStream("./screenshot.jpg");
+    reply.type("image/jpeg");
+    reply.send(stream);
+    return reply;
   } catch (err) {
-    console.log(err)
-    return {}
+    global.log(err);
+    throw err;
   }
+});
 
-})
+server.put("/update-ss", async () => {
+  try {
+    const puppy = await Puppy.create();
+    await puppy.goto(constants.url, { props: constants.props });
+    await puppy.ss(constants.ss_name);
+    await puppy.close();
+    return { message: "updated" };
+  } catch (err) {
+    global.log(err);
+    throw err;
+  }
+});
 
 const startListening = async () => {
   try {
-    await server.listen({ port: +process.env.PORT })
+    await server.listen({ port: +process.env.PORT });
   } catch (err) {
-    server.log.error(err)
-    process.exit(1)
+    server.log.error(err);
+    process.exit(1);
   }
-}
-startListening()
+};
+startListening();
---
+++ utils.js
@@ -0,0 +1,5 @@
+export const constants = {
+  url: "https://dev.to/",
+  props: { waitUntil: "networkidle0" },
+  ss_name: "screenshot.jpg",
+};
